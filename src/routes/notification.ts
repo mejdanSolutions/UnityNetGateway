1ce1a17b5d25b12af73a77fc21458d2ff149095e
Faruk Spahic <farukspahictz@gmail.com>
2023-04-21 22:40:56 +0200

    Added a query which marks notifications as read

--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -2,12 +2,15 @@ import express from "express";
 import {
   createNotification,
   getNotifications,
+  markNotificationAsRead,
 } from "../controllers/notification";
 import { protect } from "../utils/protect";
 
 const router = express.Router();
 
 router.get("/getNotifications", protect, getNotifications);
+
+router.put("/markNotificationAsRead", protect, markNotificationAsRead);
 
 router.post(
   "/createNotification/:receiverId/:postId",
